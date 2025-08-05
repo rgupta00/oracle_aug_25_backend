5938277c4488137001e2d8ece771dfc173cfbab5
rgupta00 <rgupta.mtech@gmail.com>
2025-08-05 10:38:17 +0530

    jquery basics is done

--- spring_boot/fullstack/boot/empappajax/src/main/java/com/empapp/controller/EmployeeController.java
+++ spring_boot/fullstack/boot/empappajax/src/main/java/com/empapp/controller/EmployeeController.java
@@ -1,12 +1,13 @@
 package com.empapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.empapp.repo.EmployeeRepo;
-
+@CrossOrigin("*")
 @RestController
 public class EmployeeController {
 
